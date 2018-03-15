22c906805a410e32bf975bc2e954a9d16a96bfdd
Wyatt Johnson <wyattjoh@gmail.com>
2018-03-15 11:53:02 -0600

    added resolver

--- resolver.js
+++ resolver.js
@@ -1,3 +1,30 @@
-// This file we'll create our resolver in to override how assets are loaded!
+// We'll need to modify the behavior of how assets are
+// "resolved" in Talk, so we override the base asset resolver
+// for the RootQuery type.
+module.exports = {
+  RootQuery: {
+    asset: async (root, args, ctx) => {
+      // We'll grab the id of the asset being requested
+      // such that we'll be able to lookup the asset.
+      const { id } = args;
+      if (!id) {
+        // If the ID isn't provided, we don't want to do
+        // anything.
+        return null;
+      }
 
-module.exports = {};
+      // A mouthful for sure, but we need to use the loader
+      // that is available on the graph context in order to
+      // lookup the asset by ID.
+      const asset = await ctx.loaders.Assets.getByID.load(id);
+      if (!asset) {
+        // If the asset can't be found, we don't want to do
+        // anything.
+        return null;
+      }
+
+      // Send the asset back.
+      return asset;
+    },
+  },
+};
